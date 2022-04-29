fedbd8567ad6acd0df6f7018fbb450303ae986d6
Ashly Mayle <infiernodeashly@gmail.com>
2022-04-29 17:05:46 -0400

    remove client/build from ignore file.

---
+++ client/build/precache-manifest.ff067d165606ca211f140c9471012801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b2431466abd5a79186f0d4308e03f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2353b78e3cb309fafefa",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "6e6e3dcc3685b287eaad",
+    "url": "/static/js/2.8a709052.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.8a709052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2353b78e3cb309fafefa",
+    "url": "/static/js/main.97bba6fc.chunk.js"
+  },
+  {
+    "revision": "b13f22e3823e30886374",
+    "url": "/static/js/runtime-main.28b4676d.js"
+  }
+]);
