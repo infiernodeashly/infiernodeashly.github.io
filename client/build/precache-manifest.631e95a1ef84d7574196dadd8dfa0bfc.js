f27ab85d351a1855fa67dcc315edfb1e3f959ff7
Ashly Mayle <infiernodeashly@gmail.com>
2022-04-29 18:01:11 -0400

    update

---
+++ client/build/precache-manifest.631e95a1ef84d7574196dadd8dfa0bfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc473051d98a59b4a9a0b92ca689466",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2353b78e3cb309fafefa",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "6e6e3dcc3685b287eaad",
+    "url": "/static/js/2.8a709052.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.8a709052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2353b78e3cb309fafefa",
+    "url": "/static/js/main.97bba6fc.chunk.js"
+  },
+  {
+    "revision": "b13f22e3823e30886374",
+    "url": "/static/js/runtime-main.28b4676d.js"
+  }
+]);
