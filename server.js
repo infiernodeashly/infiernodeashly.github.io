d92386e974c027f67cb45fdedffb8d57475cecdd
Ashly Mayle <infiernodeashly@gmail.com>
2022-04-29 22:17:28 -0400

    update server file to add Access control allow

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
